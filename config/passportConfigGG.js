b002ee99aa481e4f15593340f2c94a92bb19cd20
kiet-444 <kietcao4444@gmail.com>
2024-08-03 16:47:26 +0700

    m

---
+++ config/passportConfigGG.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const Staff = require('../model/staffModel');
+require('dotenv').config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    //   callbackURL: "/staffsRouter/auth/google/callback"
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await Staff.findOne({ googleId: profile.id });
+        if (!user) {
+          user = await Staff.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            role: 'staff',
+          });
+        }
+        done(null, user);
+      } catch (err) {
+        done(err, null);
+      }
+    }
+  )
+);
+
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  Staff.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+module.exports = passport;
---
+++ config/uploadMiddleware.js
@@ -0,0 +1,26 @@
+const multer = require('multer');
+// dùng multer 
+const storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { files: 3 } 
+});
+s
+// Middleware kiểm tra số lượng tệp
+function limitFileCount(req, res, next) {
+    if (req.files && req.files.length > 3) {
+        console.log('Too many files');
+        return res.status(413).json({
+            success: false,
+            message: 'You can upload up to 3 images only'
+        });
+    }
+    next();
+}
+
+module.exports = { upload, limitFileCount };
